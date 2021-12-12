7e8f459a01db2034a8b99b3d6a3320209fde51d8
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-12-12 16:52:05 +0800

    test

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: vercel(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
+
+export default config;
