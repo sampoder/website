9aa14d19b6c3a5e996a20417d6da1d0ec16e2f3e
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-12-12 16:53:03 +0800

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-<<<<<<< HEAD
 import vercel from '@sveltejs/adapter-vercel';
-=======
->>>>>>> 050cc3ba31298a4c884d99dda29a2a180ee4339c
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,11 +9,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-<<<<<<< HEAD
 		adapter: vercel(),
-=======
-		adapter: adapter(),
->>>>>>> 050cc3ba31298a4c884d99dda29a2a180ee4339c
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
