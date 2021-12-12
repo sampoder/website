96b74d6af9c060d9d62e798891b7034d121d1cd8
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-12-12 16:58:29 +0800

    Update meta.svelte

--- src/components/meta.svelte
+++ src/components/meta.svelte
@@ -22,4 +22,4 @@
 	<link rel="apple-touch-icon" sizes="180x180" href={`/favicon.png`} />
 	<link rel="icon" type="image/png" sizes="32x32" href={`/favicon.png`} />
 	<link rel="icon" type="image/png" sizes="16x16" href={`/favicon.png`} />
-</svelte:head>>
+</svelte:head>
