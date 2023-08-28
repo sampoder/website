fe3f7b91abe741f965164fd27284e0e4a94e7906
Sam Poder <sam@hackclub.com>
2023-08-27 17:13:39 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -7,28 +7,7 @@ const config = {
 
 	kit: {
 		adapter: vercel(),
-		target: '#svelte',
-
-	    	hooks: {
-	      		async handle({ request, resolve }) {
-			const referer = request.headers.referer || '';
-			const refererHost = new URL(referer).hostname;
-	
-			if (refererHost.includes('samsmomis.gay')) {
-			  const redirectUrl = `https://hackoc.org${request.path}`;
-			  return {
-			    status: 301,
-			    headers: {
-			      location: redirectUrl,
-			    },
-			  };
-			}
-	
-			const response = await resolve(request);
-	
-			return response;
-	      		},
-	    	},
+		target: '#svelte'
 	}
 };
 
