d8996e4d355eb0c758e3da063cf0488191687cd5
Sam Poder <sam@hackclub.com>
2023-08-27 17:15:58 -0700

    Update hooks.server.js

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,18 +1,19 @@
 async handle({ request, resolve }) {
-			const referer = request.headers.referer || '';
-			const refererHost = new URL(referer).hostname;
+	const referer = request.headers.referer || '';
+	console.log(referer)
+	const refererHost = new URL(referer).hostname;
 	
-			if (refererHost.includes('samsmomis.gay')) {
-			  const redirectUrl = `https://hackoc.org${request.path}`;
-			  return {
-			    status: 301,
-			    headers: {
-			      location: redirectUrl,
-			    },
-			  };
-			}
+	if (refererHost.includes('samsmomis.gay')) {
+	  const redirectUrl = `https://hackoc.org${request.path}`;
+	  return {
+	    status: 301,
+	    headers: {
+	      location: redirectUrl,
+	    },
+	  };
+	}
 	
-			const response = await resolve(request);
+	const response = await resolve(request);
 	
-			return response;
+	return response;
 }
