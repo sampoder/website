b6e7f92a8693a5793df7d80f777f35f00922a78d
Sam Poder <sam@hackclub.com>
2025-02-15 18:51:34 -0600

    Update it to the recording version

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -39,7 +39,7 @@
 <div class="grid-2">
 	<iframe 
 		height="300"
-		src="https://www.youtube.com/embed/M25fETp83jQ?si=WAxDdJVtXiMw1wmn&amp;start=5211" 
+		src="https://www.youtube.com/embed/CBxilReUkJ0?si=GgcQ9GDoqSl1avoK;start=3005" 
 		title="YouTube video player" 
 		frameborder="0" 
 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
